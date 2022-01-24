1117d357cccbdf7d7872cdf57e86c0e0e4b75d5c
Tom Goundry <atumtum12@gmail.com>
2022-01-24 23:56:04 +0000

    initial commit

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
+app.commandLine.appendSwitch("disable-gpu")
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -9,11 +10,13 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 678,
+    height: 90,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true
+    },
+    frame:false
   })
 
   // and load the index.html of the app.
